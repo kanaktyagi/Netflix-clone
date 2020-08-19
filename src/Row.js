55d181189c33b4fe1038476e8c01337fd158ef42
Kanak Tyagi <tyagiknk.india@gmail.com>
2020-08-19 12:58:55 +0530

    Created a row component which we will have all the container displaying the perticular section

---
+++ src/Row.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Row({title}) {
+    return (
+        <div>
+            {/* title */}
+            <h2>{title}</h2>
+            {/* container -> poster  */}
+        </div>
+    )
+}
+
+export default Row
