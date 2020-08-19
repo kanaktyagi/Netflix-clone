b811de3551f2a490dd60d04ef306cd3be7905696
Kanak Tyagi <tyagiknk.india@gmail.com>
2020-08-19 13:30:19 +0530

    Added the fetchUrl property to pass it as props to Row component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import Row from './Row' 
+import Request from './request'
 
 function App() {
   return (
     <div className="App">
       <h1>Heyy</h1>
-      <Row title="NETFLIX Original"/>
-      <Row title="Treading Now"/>
+      <Row title="NETFLIX Original" fetchUrl={Request.fetchNetFlixOriginals}/>
+      <Row title="Treading Now" fetchUrl={Request.fetchTrending}/>
     </div>
   );
 }
