83ea4604bcad8a9379e887be39b1be9eb5e2115a
Kanak Tyagi <tyagiknk.india@gmail.com>
2020-08-19 13:00:30 +0530

    Imported  the Row component into App.js and passed the title as a props to Row component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Row from './Row' 
 
 function App() {
   return (
     <div className="App">
       <h1>Heyy</h1>
+      <Row title="NETFLIX Original"/>
+      <Row title="Treading Now"/>
     </div>
   );
 }
