34e75136b83feb6b27532d35a6908735e02dae0e
Kanak Tyagi <tyagiknk.india@gmail.com>
2020-08-19 18:44:18 +0530

    Added a prop called isLargeRow so as to make the same component display one row bigger than the others

--- src/App.js
+++ src/App.js
@@ -7,8 +7,17 @@ function App() {
   return (
     <div className="App">
       <h1>Heyy</h1>
-      <Row title="NETFLIX Original" fetchUrl={Request.fetchNetFlixOriginals} />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={Request.fetchNetFlixOriginals}
+        isLargeRow
+      />
       <Row title="Treading Now" fetchUrl={Request.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={Request.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={Request.fetchActionMovies} />
+      <Row title="Comedy movies" fetchUrl={Request.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={Request.fetchHorrorMovies} />
+      <Row title="Documentaries" fetchUrl={Request.fetchDocumentaries} />
     </div>
   );
 }
