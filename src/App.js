9dbb72783adc163b41a155b8bdfcb0a07a1e3e1c
Kanak Tyagi <tyagiknk.india@gmail.com>
2020-08-19 16:27:48 +0530

    Added the image url to display the movies in the flex view

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import './App.css';
-import Row from './Row' 
-import Request from './request'
+import React from "react";
+import "./App.css";
+import Row from "./Row";
+import Request from "./request";
 
 function App() {
   return (
     <div className="App">
       <h1>Heyy</h1>
-      <Row title="NETFLIX Original" fetchUrl={Request.fetchNetFlixOriginals}/>
-      <Row title="Treading Now" fetchUrl={Request.fetchTrending}/>
+      <Row title="NETFLIX Original" fetchUrl={Request.fetchNetFlixOriginals} />
+      <Row title="Treading Now" fetchUrl={Request.fetchTrending} />
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -1,26 +1,38 @@
-import React,{useState,useEffect} from 'react'
-import axios from './axios'
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import "./row.css";
 
+const base_url = "https://image.tmdb.org/t/p/original/";
 
-function Row({title, fetchUrl}) {
-    const [movies, setMovies] = useState([])
-  
-    useEffect(() => {
-        async function fetchDate() {
-           const request = await axios.get(fetchUrl)
-           console.log(request)
-           return request 
-        }
-        fetchDate();
-    },[]);
+function Row({ title, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
 
-    return (
-        <div>
-            {/* title */}
-            <h2>{title}</h2>
-            {/* container -> poster  */}
-        </div>
-    )
+  useEffect(() => {
+    async function fetchDate() {
+      const request = await axios.get(fetchUrl);
+      console.log(request.data.results);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchDate();
+  }, [fetchUrl]);
+  console.log(movies);
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row_posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className="row_poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Row
+export default Row;
